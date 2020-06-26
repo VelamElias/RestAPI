380226ed4c51abbdd47fb46ca5c83aa7099c272b
VelamElias <elnatasource@gmail.com>
2020-06-26 15:02:14 -0300

    trata erros para o metodo GET

--- primeira_api.py
+++ primeira_api.py
@@ -13,16 +13,27 @@ devList = [
     }
 ]
 
-@app.route('/<int:id>/', methods=['GET', 'PUT'])
+@app.route('/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
 def desenvolvedor(id):
     if request.method == 'GET':
-        dev = devList[id]
-        return jsonify(dev)
-        
+        try:
+            response = devList[id]
+        except IndexError:
+            response = {'status' : 'erro', 'mensagem' : 'Dev com ID #''{} não existe'}
+        except Exception:
+            mensagem = 'Undefined error. Report to developer'
+            response = {'status': 'erro', 'mensagem': mensagem}
+        return jsonify(response)
+
+
     elif request.method == 'PUT':
         dados = json.loads(request.data)
         dev[id] = dados
         return jsonify(dados)
+    
+    elif request.method == 'DELETE':
+        dev.pop(id)
+        return jsonify({'status' : 'sucess', 'mensagem' : 'Registro deletado'})
 
 if __name__ == '__main__':
     app.run(debug=True)

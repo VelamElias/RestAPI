120434d85b74b3609dc01abcb945fa388ce391b9
VelamElias <elnatasource@gmail.com>
2020-06-26 14:40:58 -0300

    create list with developers

--- primeira_api.py
+++ primeira_api.py
@@ -3,5 +3,19 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
+devList = [
+    {'nome': 'Dev Severino', 
+    'habilidade' : ['Python','Java']
+    },
+    {'nome': 'Dev Isac', 
+    'habilidade' : ['C','Cpp']
+    }
+]
+
+@app.route('/<int:id>/')
+def desenvolvedor(id):
+    dev = devList[id]
+    return jsonify(dev)
+
 if __name__ == '__main__':
     app.run(debug=True)
